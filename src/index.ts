9512d9ca141c703aec950fbd9afed4a626f1dfcf
Mykal Machon <mykalmachon@gmail.com>
2020-11-05 23:15:24 -0800

    add basic export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-console.log('this is a test');
+import SmolCart from './utils/smolcart';
+
+window.SmolCart = SmolCart;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 declare global {
   interface Window {
+    SmolCart: unknown;
     SmolPolyLoaded: unknown;
   }
 }
