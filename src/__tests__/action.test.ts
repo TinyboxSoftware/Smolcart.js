70f360ab1d238781655027eb1872d87a004173d2
Mykal Machon <mykalmachon@gmail.com>
2020-12-05 14:50:00 -0800

    Chore: Laying out tests to be written

---
+++ src/__tests__/action.test.ts
@@ -0,0 +1,16 @@
+
+it('Adds Items to Cart', () => {
+  expect(true).toEqual(true);
+});
+
+it('Returns the contents of the cart', () => {
+  expect(true).toEqual(true);
+});
+
+it('Modifies the items in the cart', () => {
+  expect(true).toEqual(true);
+});
+
+it('Clears the contents in the cart', () => {
+  expect(true).toEqual(true);
+});
---
+++ src/__tests__/smolcart.test.ts
@@ -0,0 +1,29 @@
+// TODO test polyfill init and some other goodies in smolcart.js here
+
+it('Initializes and returns a valid object', () => {
+  expect(true).toEqual(true);
+});
+
+it('Loads polyfills when needed', () => {
+  expect(true).toEqual(true);
+});
+
+it("Does't load polyfills when not needed", () => {
+  expect(true).toEqual(true);
+});
+
+it('Executes add to cart', () => {
+  expect(true).toEqual(true);
+});
+
+it('Executes modify cart', () => {
+  expect(true).toEqual(true);
+});
+
+it('Executes get cart', () => {
+  expect(true).toEqual(true);
+});
+
+it('Executes clear cart', () => {
+  expect(true).toEqual(true);
+});
